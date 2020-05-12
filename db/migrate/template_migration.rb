0516bda17dc537aaf0028f5128f7d0d075d54fa9
wilfison <wilfisonbatista@gmail.com>
2020-05-12 11:36:05 -0300

    adiciona timestamps

--- db/migrate/template_migration.rb
+++ db/migrate/template_migration.rb
@@ -4,6 +4,8 @@ class TemplateMigration < ActiveRecord::Migration[6.0]
   def change
     create_table :template_migration, force: :cascade do |t|
       # content
+
+      t.timestamps null: false
     end
   end
 end
