de520cce0eb5d7b55a166860b8f36dec55d87676
wilfison <wilfisonbatista@gmail.com>
2020-05-15 12:07:29 -0300

    adiciona modelo de mapper

---
+++ app/models/mapper/new_model_template_mapper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Mapper
+  class NewModelTemplateMapper < ApplicationRecord
+    self.table_name = 'new_model'
+
+    # alias_attribute :field_name, :field_column_name
+    #attributes
+  end
+end
--- app/models/new_model_template.rb
+++ app/models/new_model_template.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class NewModelTemplate
+class NewModelTemplate < ApplicationRecord
   # belongs_to
 
   # has_one
