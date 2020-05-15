f67c092375242a12c005ad322a067471de418694
wilfison <wilfisonbatista@gmail.com>
2020-05-15 14:40:37 -0300

    adiciona feather icons no template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 module ApplicationHelper
   def title(page_title)
@@ -7,5 +7,15 @@ module ApplicationHelper
 
   def hide_default_layout
     content_for(:hide_default_layout) { 'hide' }
+  end
+
+  def icon(name, html_options = {})
+    content_class = "fe fe-#{name}"
+    content_class << " #{html_options[:class]}" if html_options.key?(:class)
+    html_options[:class] = content_class
+
+    html = content_tag(:i, nil, html_options)
+
+    html
   end
 end
