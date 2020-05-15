17db8266c1a2af442ace31cb6245009e3c671484
wilfison <wilfisonbatista@gmail.com>
2020-05-15 14:23:11 -0300

    adiciona modelo de queries e filter

---
+++ app/queries/model_template_queries.rb
@@ -0,0 +1,29 @@
+class ModelTemplateQueries
+  attr_reader :q
+
+  def initialize(params = {})
+    @params = params
+    @q = scope_query.ransack(params[:q])
+  end
+
+  def to_relation
+    scope = q
+      .result
+      .page(params[:page])
+      .per(30)
+      .without_count
+
+    scope = scope.order(:id) if q.sorts.empty?
+    
+    scope
+  end
+
+  private
+  attr_reader :params
+
+  def scope_query
+    # return current_user.model_template if current_user.present?
+
+    ModelTemplate
+  end
+end
